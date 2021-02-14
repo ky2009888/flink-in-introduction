72b222183804fd696ea085eac101942af51862a1
cf_jijinliang <ji_jinliang2012@163.com>
2021-02-15 00:29:54 +0800

    演示flink中自定义数据源

--- src/main/java/com/flink/apps/datasource/KafkaSourceFunction.java
+++ src/main/java/com/flink/apps/datasource/KafkaSourceFunction.java
@@ -1,23 +1,30 @@
 package com.flink.apps.datasource;
 
+import cn.hutool.core.util.RandomUtil;
 import lombok.AllArgsConstructor;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 /**
  * @Author ky2009666
- * @Description 自定义kafka的数据源
+ * @Description 自定义flink的数据源.
  * @Date 2021/2/14
  **/
 @AllArgsConstructor
-public class KafkaSourceFunction implements SourceFunction {
+public class KafkaSourceFunction implements SourceFunction<String> {
+    /**
+     * 是否执行的标记，默认是true,代表执行.
+     */
+    private boolean isRunning = true;
 
     @Override
     public void run(SourceContext ctx) throws Exception {
-
+        while (isRunning) {
+            ctx.collect(RandomUtil.randomNumbers(10));
+        }
     }
 
     @Override
     public void cancel() {
-
+        isRunning = false;
     }
 }
